6cac962f568c4f53fe60fe6711bbf1c9587f0610
Cyrus <CyrusNES@proton.com>
2024-03-19 18:14:18 +0700

    add linux fnm support

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,1 +1,9 @@
-fnm env --use-on-cd | source
+switch (uname)
+ case Darwin
+   source fnm env --use-on-cd | source
+ case Linux
+   set PATH "/home/cyrus/.local/share/fnm" $PATH
+   fnm env | source
+ case '*'
+   echo 'No OS found!!'
+end
