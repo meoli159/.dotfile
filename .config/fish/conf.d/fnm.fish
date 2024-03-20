d4ccec7e08ece1d4168e27d2e2e86dcf42b4f36a
Cyrus <CyrusNES@proton.me>
2024-03-21 00:01:56 +0700

    update fnm (linux using homebrew)

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,9 +1,1 @@
-switch (uname)
-    case Darwin
-        fnm env --use-on-cd | source
-    case Linux
-        set PATH "/home/cyrus/.local/share/fnm" $PATH
-        fnm env | source
-    case '*'
-        echo 'No OS found!!'
-end
+fnm env --use-on-cd | source
